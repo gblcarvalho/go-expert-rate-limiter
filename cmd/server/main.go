ebb0cad5419d766d9c78337a1a95a3ef11d40154
Gabriel Miranda Carvalho <gabrielm.car@gmail.com>
2025-04-18 13:00:06 -0300

    feat: creating function to get the ip and api key token

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 )
 
+func getIPAddress(r *http.Request) string {
+	ips := r.Header.Get("X-Forwarded-For")
+	if ips != "" {
+		ipsArr := strings.Split(ips, ",")
+		return strings.TrimSpace(ipsArr[0])
+	}
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return r.RemoteAddr
+	}
+	return ip
+}
+
+func getAPIKEYToken(r *http.Request) string {
+	return r.Header.Get("API_KEY")
+}
+
 func helloWorldMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hello World")
+		fmt.Printf("Hello World from: %s and Token: %s", getIPAddress(r), getAPIKEYToken(r))
 		next.ServeHTTP(w, r)
 	})
 }
